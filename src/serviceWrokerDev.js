802d9226390643c297dd700bfc939c9c4c11fc98
Atanu Kumar Dey <atanukumardey_2020@gift.edu.in>
2023-05-24 16:07:35 +0530

    small chages in service worker console line

--- src/serviceWrokerDev.js
+++ src/serviceWrokerDev.js
@@ -1,7 +1,7 @@
 export default function register() {
     let swDev = `/serviceWorker.js`;
     navigator.serviceWorker.register(swDev).then((result) => {
-        console.log('result', result);
+        console.log("Service Worker successfully connected");
     }).catch((err) => {
         console.log('err', err);
     });
